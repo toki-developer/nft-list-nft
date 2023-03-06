923ff5957cfb50e91ce2f5577103d9206bd54912
Toki <55911995+tokio-k@users.noreply.github.com>
2023-03-07 08:53:04 +0900

    animation作成

--- contracts/MyNFTsNFT.sol
+++ contracts/MyNFTsNFT.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import "hardhat/console.sol";
 
 
 contract MyNFTsNFT is ERC721, Ownable {
@@ -11,23 +12,15 @@ contract MyNFTsNFT is ERC721, Ownable {
     uint256 private _nextTokenId = 1;
     constructor() ERC721("MyNFTsNFT", "MYNFT") {}
 
-    function buildImage(uint256 tokenId) public view returns(string memory) {
-        address nftOwner = ownerOf(tokenId);
-        return Base64.encode(bytes(abi.encodePacked(
-            '<svg width="600" height="600" xmlns="http://www.w3.org/2000/svg">',
-            nftOwner,
-            '</svg>'
-        )));
-    }
-
      function tokenURI(uint256 tokenId) public view virtual override returns (string memory){
         require(_exists(tokenId), "URI query for nonexistent token");
+        address nftOwner = ownerOf(tokenId);
 
         return string(
             abi.encodePacked(
                 'data:application/json;base64,',
                 Base64.encode(bytes(abi.encodePacked(
-                    '{"name":"My NFTs NFT #',  Strings.toString(tokenId),'","description": "You can see your NFTs.","image": "data:image/svg;base64,',buildImage(tokenId),'"}')))
+                    '{"name":"My NFTs NFT #',  Strings.toString(tokenId),'","description": "You can see your NFTs.","image": "https://gr2v2wtpgux5jwj5aq6vwu5iaoxv5vub5fihr5ul3irmp3rvie7a.arweave.net/NHVdWm81L9TZPQQ9W1OoA69e1oHpUHj2i9oix-41QT4", "animation_url": "https://arweave.net/6rzPT_WuZxY_BG8bwk28eOKXbQflmsXcquus-uUt4TU?nft-owner=',Strings.toHexString(uint256(uint160(nftOwner))),'"}')))
             )
         );
     }
