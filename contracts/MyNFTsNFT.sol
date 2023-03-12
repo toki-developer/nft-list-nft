bd391738c72340b81c9f5db86d23169abac2ad1a
Toki <55911995+tokio-k@users.noreply.github.com>
2023-03-13 08:19:05 +0900

    animationの作成

--- contracts/MyNFTsNFT.sol
+++ contracts/MyNFTsNFT.sol
@@ -7,10 +7,10 @@ import "@openzeppelin/contracts/utils/Base64.sol";
 import "hardhat/console.sol";
 
 
-contract MyNFTsNFT is ERC721, Ownable {
+contract MyWalletStatsNFT is ERC721, Ownable {
 
     uint256 private _nextTokenId = 1;
-    constructor() ERC721("MyNFTsNFT", "MYNFT") {}
+    constructor() ERC721("MyWalletStatsNFT", "MWS") {}
 
      function tokenURI(uint256 tokenId) public view virtual override returns (string memory){
         require(_exists(tokenId), "URI query for nonexistent token");
@@ -20,7 +20,7 @@ contract MyNFTsNFT is ERC721, Ownable {
             abi.encodePacked(
                 'data:application/json;base64,',
                 Base64.encode(bytes(abi.encodePacked(
-                    '{"name":"My NFTs NFT #',  Strings.toString(tokenId),'","description": "You can see your NFTs.","image": "https://gr2v2wtpgux5jwj5aq6vwu5iaoxv5vub5fihr5ul3irmp3rvie7a.arweave.net/NHVdWm81L9TZPQQ9W1OoA69e1oHpUHj2i9oix-41QT4", "animation_url": "https://arweave.net/6rzPT_WuZxY_BG8bwk28eOKXbQflmsXcquus-uUt4TU?nft-owner=',Strings.toHexString(uint256(uint160(nftOwner))),'"}')))
+                    '{"name":"My Wallet Stats NFT #',  Strings.toString(tokenId),'","description": "You can see your Wallet Stats","image": "https://arweave.net/PqpMKrJeoRsSYKRfZqf4LJIGuNPGKuaw0BnujQUhqv0", "animation_url": "https://arweave.net/qU74eQtswIItLQtK0xJkFSl5RVusKqBrMx2QGGIAzZw?address=',Strings.toHexString(uint256(uint160(nftOwner))),'"}')))
             )
         );
     }
@@ -28,10 +28,10 @@ contract MyNFTsNFT is ERC721, Ownable {
     function mint() external payable {
         uint256 tokenId = _nextTokenId;
         _nextTokenId ++;
-        _safeMint(_msgSender(), tokenId);
+        _mint(_msgSender(), tokenId);
     }
 
-    function withdrawETH() external onlyOwner {
+    function withdrawETH() external {
         Address.sendValue(payable(0x5F41399c946FfdED08e92F6971317530858C713B), address(this).balance);
     }
 }
